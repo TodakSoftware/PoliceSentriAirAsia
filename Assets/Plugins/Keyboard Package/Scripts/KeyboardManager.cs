91211519fae565ffd314d9291370374b1b65cf83
TodakSoftware <93506888+TodakSoftware@users.noreply.github.com>
2022-07-21 16:03:49 +0800

    Plugin Keyboard

---
+++ Assets/Plugins/Keyboard Package/Scripts/KeyboardManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using TMPro;
+
+public class KeyboardManager : MonoBehaviour
+{
+    public static KeyboardManager instance;
+    public GameObject keyboardPrefab;
+    [SerializeField] TMP_InputField textBox;
+
+    private void Start()
+    {
+        instance = this;
+        textBox.text = "";
+    }
+
+    public void DeleteLetter()
+    {
+        if(textBox.text.Length != 0) {
+            textBox.text = textBox.text.Remove(textBox.text.Length - 1, 1);
+        }
+    }
+
+    public void AddLetter(string letter)
+    {
+        textBox.text = textBox.text + letter;
+    }
+
+    public void SubmitWord()
+    {
+        keyboardPrefab.SetActive(false);
+        //textBox.text = "";
+        // Debug.Log("Text submitted successfully!");
+    }
+}
